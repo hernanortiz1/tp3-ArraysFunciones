63bb8332e421f6646a271eed82504a6d718a950e
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-25 10:24:28 -0300

    complete ejercicio hasta guardar suma en array

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -5,3 +5,22 @@ de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y
 anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces
 esta operación.
 */
+
+const apariciones = [];
+for (let i = 0; i < 50; i++) {
+  let suma = 0;
+  let dado1 = Math.floor(Math.random() * 5) + 1;
+  let dado2 = Math.floor(Math.random() * 5) + 1;
+  console.log(dado1);
+  console.log(dado2);
+  suma += dado1 + dado2;
+
+  apariciones.push(suma);
+}
+
+document.writeln(`Datos del array`);
+document.writeln(`<ul>`);
+for(let i=0; i < apariciones.length; i++){
+    document.writeln(`<li>suma ${apariciones[i]} apariciones</li>`); 
+}
+document.writeln(`</ul>`);
